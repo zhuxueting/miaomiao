187c78be4940295389419a48b7d29d657df614b2
zhuzhu <670582904@qq.com>
2020-07-31 16:11:25 +0800

    add createComponents

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,24 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Router from 'vue-router'
+import movieRouter from './movie'
+import cinemaRouter from './cinema'
+import mineRouter from './mine'
+// import adminRouter from './admin'
 
-Vue.use(VueRouter)
+Vue.use(Router)
 
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
-const router = new VueRouter({
+export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes: [
+    movieRouter,
+    cinemaRouter,
+    mineRouter,
+    // adminRouter,
+    {
+      path : '/*',
+      redirect : '/movie'
+    }
+  ]
 })
 
-export default router
